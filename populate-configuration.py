23de6c0d3825c98d947ff74325a536f75365d836
Will Laws <will.laws@me.com>
2024-06-15 14:38:41 +1000

    Add support for v4 ECU

--- populate-configuration.py
+++ populate-configuration.py
@@ -1,8 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-def generate_yaml():
-    url = 'http://IP-OF-ECU-3/cgi-bin/parameters'
+# Usage
+#url_old = 'http://IP-OF-ECU-3/cgi-bin/parameters'
+#url_new = 'http://IP-OF-ECU-4/index.php/realtimedata'
+url = 'http://10.40.0.235/cgi-bin/parameters'
+
+def generate_yaml(url):
     r = requests.get(url)
     
     if r.status_code != 200:
@@ -19,12 +23,21 @@ def generate_yaml():
     rows = table.find_all('tr')
     inverter_ids = []
 
+    is_new_version = False
+    if 'rowspan' in str(rows[1]):
+        is_new_version = True
+
     for i, row in enumerate(rows):
         columns = row.find_all('td')
         if len(columns) == 6:
             if i == 0:
                 continue
-            inverter_ids.append(columns[0].text)
+            inverter_ids.append(columns[0].text.strip())
+        elif is_new_version and len(columns) == 5:
+            inverter_ids.append(columns[0].text.strip())
+
+    inverter_ids = list(set(inverter_ids))
+    inverter_ids.sort()
 
     print(f'Found {len(inverter_ids)} inverters')
 
@@ -35,19 +48,36 @@ def generate_yaml():
             f'    sensor:\n'
         )
         for i, id in enumerate(inverter_ids, start=1):
-            f.write(
-                f'      - name: "Solar Panel {str(i).zfill(2)} power"\n'
-                f'        value_template: \'{{{{ value_json["{id}"][0] }}}}\'\n'
-                f'        unit_of_measurement: "W"\n\n'
-                f'      - name: "Solar Panel {str(i).zfill(2)} grid frequency"\n'
-                f'        value_template: \'{{{{ value_json["{id}"][1] }}}}\'\n'
-                f'        unit_of_measurement: "Hz"\n\n'
-                f'      - name: "Solar Panel {str(i).zfill(2)} grid voltage"\n'
-                f'        value_template: \'{{{{ value_json["{id}"][2] }}}}\'\n'
-                f'        unit_of_measurement: "V"\n\n'
-                f'      - name: "Solar Panel {str(i).zfill(2)} temperature"\n'
-                f'        value_template: \'{{{{ value_json["{id}"][3] }}}}\'\n'
-                f'        unit_of_measurement: "°C"\n\n'
-            )
+            if is_new_version:
+                f.write(
+                    f'      - name: "Solar Panel {str(i).zfill(2)} power"\n'
+                    f'        value_template: \'{{{{ value_json["{id}"][0] }}}}\'\n'
+                    f'        unit_of_measurement: "W"\n\n'
+                    f'      - name: "Solar Panel {str(i).zfill(2)} grid frequency"\n'
+                    f'        value_template: \'{{{{ value_json["{id[:-2]}"][1] }}}}\'\n'
+                    f'        unit_of_measurement: "Hz"\n\n'
+                    f'      - name: "Solar Panel {str(i).zfill(2)} grid voltage"\n'
+                    f'        value_template: \'{{{{ value_json["{id}"][2] }}}}\'\n'
+                    f'        unit_of_measurement: "V"\n\n'
+                    f'      - name: "Solar Panel {str(i).zfill(2)} temperature"\n'
+                    f'        value_template: \'{{{{ value_json["{id[:-2]}"][3] }}}}\'\n'
+                    f'        unit_of_measurement: "°C"\n\n'
+                )
+            else:
+                f.write(
+                    f'      - name: "Solar Panel {str(i).zfill(2)} power"\n'
+                    f'        value_template: \'{{{{ value_json["{id}"][0] }}}}\'\n'
+                    f'        unit_of_measurement: "W"\n\n'
+                    f'      - name: "Solar Panel {str(i).zfill(2)} grid frequency"\n'
+                    f'        value_template: \'{{{{ value_json["{id}"][1] }}}}\'\n'
+                    f'        unit_of_measurement: "Hz"\n\n'
+                    f'      - name: "Solar Panel {str(i).zfill(2)} grid voltage"\n'
+                    f'        value_template: \'{{{{ value_json["{id}"][2] }}}}\'\n'
+                    f'        unit_of_measurement: "V"\n\n'
+                    f'      - name: "Solar Panel {str(i).zfill(2)} temperature"\n'
+                    f'        value_template: \'{{{{ value_json["{id}"][3] }}}}\'\n'
+                    f'        unit_of_measurement: "°C"\n\n'
+                )
 
-generate_yaml()
+
+generate_yaml(url)
--- solarhtml2json.py
+++ solarhtml2json.py
@@ -5,6 +5,7 @@ import json
 
 def get_power_data():
     url = 'http://IP-OF-ECU-3/cgi-bin/parameters'
+    #url = 'http://IP-OF-ECU-4/index.php/realtimedata'
     r = requests.get(url)
     
     if r.status_code != 200:
@@ -21,31 +22,56 @@ def get_power_data():
     rows = table.find_all('tr')
     power_data = {}
 
+    is_new_version = False
     for row in rows:
         columns = row.find_all('td')
         if len(columns) == 6:
-            data_list = [columns[i].text.replace('\xa0', ' ').replace('\xba', ' ') for i in range(1, 6)]
-            power_data[columns[0].text] = data_list
-        else :
-            print('Not a power data row')
+            is_new_version = True
+            break
+
+    if is_new_version:
+        inverter_id = None
+        for row in rows[1:]:  # Skip the header row
+            columns = row.find_all('td')
+            if len(columns) == 6:
+                inverter_id = columns[0].text.strip()
+                power_data[inverter_id] = [columns[i].text.replace('&#176;C', '').strip() for i in range(1, 6)]
+            else:
+                power_data[inverter_id][0] += f" / {columns[1].text.strip()}"
+                power_data[inverter_id][2] += f" / {columns[3].text.strip()}"
+    else:
+        for row in rows:
+            columns = row.find_all('td')
+            if len(columns) == 6:
+                data_list = [columns[i].text.replace('\xa0', ' ').replace('\xba', ' ').strip() for i in range(1, 6)]
+                power_data[columns[0].text.strip()] = data_list
+            else:
+                print('Not a power data row')
 
     # Clean the data
     latest_time = '0'
     for pannel in power_data:
         if pannel != 'Inverter ID':  # skip the row of headers
             # Remove units from the data
-            power_data[pannel][0] = power_data[pannel][0].replace(' W', '')
-            power_data[pannel][1] = power_data[pannel][1].replace(' Hz', '')
-            power_data[pannel][2] = power_data[pannel][2].replace(' V', '')
-            power_data[pannel][3] = power_data[pannel][3].replace(' oC', '')
+            if is_new_version:
+                power_data[pannel][0] = power_data[pannel][0].replace('W', '').replace('/', '').strip()
+                power_data[pannel][1] = power_data[pannel][1].replace('Hz', '').strip()
+                power_data[pannel][2] = power_data[pannel][2].replace('V', '').replace('/', '').strip()
+                power_data[pannel][3] = power_data[pannel][3].replace('°C', '').strip()
+            else:
+                power_data[pannel][0] = power_data[pannel][0].replace('W', '').strip()
+                power_data[pannel][1] = power_data[pannel][1].replace('Hz', '').strip()
+                power_data[pannel][2] = power_data[pannel][2].replace('V', '').strip()
+                power_data[pannel][3] = power_data[pannel][3].replace('oC', '').strip()
 
             # Panels go offline at night and return empty values. Mark these as 0
             # instead to prevent the HA sensor from disappearing.
-            power_data[pannel] = [d.strip() or '0' for d in power_data[pannel]]
-            
+            power_data[pannel] = ['0' if d.strip() in ['', '-'] else d.strip() for d in power_data[pannel]]
+
             # Note the latest timestamp found so we can fill it in for missing
             # panels.
             latest_time = max(latest_time, power_data[pannel][4])
+
     # For missing panels (offline over night etc) fill in the latest timestamp
     # found. If all are offline, fill in the current time.
     if latest_time == '0':
@@ -53,7 +79,7 @@ def get_power_data():
     for pannel in power_data:
         if power_data[pannel][4] == '0':
             power_data[pannel][4] = latest_time
-        
+
     with open('www/power_data.json', 'w') as outfile:
         json.dump(power_data, outfile)
 
